82b15f15108f9ff5b51ce13c4d8543f2c558e45a
NeverMind <Fry_Vetoshkin_97@ukr.net>
2019-03-28 23:07:52 +0200

    fix dropdown menu; add api request

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,53 @@
 //
 // global libs
 
+function getRequest(url) {
+    fetch(url)
+    .then((res) => {
+        if(res.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            res.status);
+            return;
+        }
+        return res.json().then(function(data) {
+            console.log(data);
+            dataHandle(data);
+        });
+    })
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+}
+
+function dataHandle(data) {
+    let nameSort = (a,b) => {
+        if(a.name > b.name) {
+            return 1;
+        }
+        if(a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    };
+    let storage = document.querySelector('.js-storage');
+    
+    let link = `
+    <a href="#" class="main__link"><i class="js-type"></i>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ducimus?</a>
+    `;
+    let arr = data.sort(nameSort)
+    .map((item) => {
+        let block = document.createElement('div');
+        block.className = 'main__letter';
+        let header = document.createElement('h3');
+        header.className = 'main__header--underlined';
+        header.innerHTML = item.name[0];
+        block.appendChild(header);
+        return block;
+    });
+    arr.sort().forEach((item) => storage.appendChild(item));
+}
+
 function sliderInit() {    
     if(!document.querySelector('.js-slider')) {
         return;
@@ -42,5 +89,6 @@ function sliderInit() {
 }
 
 window.addEventListener('load', function() {
+    getRequest('http://5c9915184236560014393204.mockapi.io/mylib/files');
     sliderInit();
 });
